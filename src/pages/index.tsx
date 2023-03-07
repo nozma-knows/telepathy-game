4cba5d0492918193282a1e62728764c02b72724f
Noah Milberger <n.milberger@gmail.com>
2023-03-07 07:22:29 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="text-4xl font-bold">Telepathy Game</div>
+    </div>
+  );
+}
