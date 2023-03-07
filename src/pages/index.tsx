dbdc5a9e9249197cb6e5d6972be5033358bdf726
Noah Milberger <n.milberger@gmail.com>
2023-03-07 12:52:19 -0500

    Working on implementing pixijs library.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,52 @@
-import { useState, useMemo, useEffect } from "react";
-import { Stage, Container, Graphics } from "@pixi/react";
+import React, { useState, useEffect } from "react";
+// import { Stage } from "@inlet/react-pixi";
+import { Stage } from "@pixi/react";
+import Draggable from "@/components/ui/Draggable";
 import useWindowSize from "@/components/utils/hooks/useWindowSize";
-
-let index = 1;
-
-const Item = (props: any) => {
-  const [myIndex, setMyIndex] = useState(1);
-
-  const color = useMemo(() => {
-    return `0x${Math.floor(
-      Math.abs(Math.sin(index++) * 16777215) % 16777215
-    ).toString(16)}`;
-  }, []);
-
-  return (
-    <Graphics
-      {...props}
-      interactive={true}
-      zIndex={myIndex}
-      pointerdown={() => setMyIndex(index++)}
-      draw={(g) => {
-        g.beginFill(+color);
-        g.drawRect(0, 0, 100, 100);
-        g.endFill();
-      }}
-    />
-  );
-};
 
 export default function Home() {
   const size = useWindowSize();
-  const [height, setHeight] = useState(0);
   const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
 
   useEffect(() => {
-    setHeight(size.height);
     setWidth(size.width);
+    setHeight(size.height);
   }, [size]);
+
   return (
-    <Stage
-      width={width}
-      height={height}
-      options={{ backgroundColor: 0x012b30 }}
-    >
-      <Container sortableChildren={true}>
-        <Item x={100} y={100} />
-        <Item x={150} y={150} />
-        <Item x={200} y={200} />
-        <Item x={250} y={250} />
-      </Container>
-    </Stage>
+    <div>
+      <Stage
+        width={width}
+        height={height}
+        options={{ backgroundColor: 0xf2b310, resolution: 2 }}
+      >
+        <Draggable
+          image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+          x={20}
+          y={20}
+        />
+        <Draggable
+          image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+          x={180}
+          y={20}
+        />
+        <Draggable
+          image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+          x={20}
+          y={180}
+        />
+        <Draggable
+          image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+          x={180}
+          y={180}
+        />
+        <Draggable
+          image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+          x={100}
+          y={100}
+        />
+      </Stage>
+    </div>
   );
 }
