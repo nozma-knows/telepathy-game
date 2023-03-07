03c7acf664c5c3d91cc149f92ad76f2d103b48df
Noah Milberger <n.milberger@gmail.com>
2023-03-07 08:16:30 -0500

    Added pixi.js and @pixi/react. Starting to play around with the libraries.

---
+++ src/components/utils/hooks/useWindowSize.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+export default function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,54 @@
-export default function Home() {
+import { useState, useMemo, useEffect } from "react";
+import { Stage, Container, Graphics } from "@pixi/react";
+import useWindowSize from "@/components/utils/hooks/useWindowSize";
+
+let index = 1;
+
+const Item = (props: any) => {
+  const [myIndex, setMyIndex] = useState(1);
+
+  const color = useMemo(() => {
+    return `0x${Math.floor(
+      Math.abs(Math.sin(index++) * 16777215) % 16777215
+    ).toString(16)}`;
+  }, []);
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="text-4xl font-bold">Telepathy Game</div>
-    </div>
+    <Graphics
+      {...props}
+      interactive={true}
+      zIndex={myIndex}
+      pointerdown={() => setMyIndex(index++)}
+      draw={(g) => {
+        g.beginFill(+color);
+        g.drawRect(0, 0, 100, 100);
+        g.endFill();
+      }}
+    />
+  );
+};
+
+export default function Home() {
+  const size = useWindowSize();
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setHeight(size.height);
+    setWidth(size.width);
+  }, [size]);
+  return (
+    <Stage
+      width={width}
+      height={height}
+      options={{ backgroundColor: 0x012b30 }}
+    >
+      <Container sortableChildren={true}>
+        <Item x={100} y={100} />
+        <Item x={150} y={150} />
+        <Item x={200} y={200} />
+        <Item x={250} y={250} />
+      </Container>
+    </Stage>
   );
 }
